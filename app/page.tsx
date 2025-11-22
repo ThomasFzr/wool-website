06df63f23ca64b49fd6d1182efb054eeceb0154f
ThomasFzr <thfltzr@gmail.com>
2025-11-22 16:27:15 +0100

    refactor(homepage): remove knit-scroll animation for cleaner layout

--- app/page.tsx
+++ app/page.tsx
@@ -274,49 +274,7 @@ export default function HomePage() {
       : creations;
 
   return (
-    <main className="relative min-h-screen overflow-hidden">
-      {/* 🧶 FOND ANIMÉ AVEC PELOTES & AIGUILLES */}
-      <div className="pointer-events-none absolute inset-0 -z-10 opacity-15">
-        <div className="flex h-full w-[300%] animate-knit-scroll">
-          {/* Dupliqué pour créer un défilement continu */}
-          <div className="flex h-full w-1/3">
-            <img
-              src="/images/bg-yarn-1.png"
-              alt="Pelotes de laine"
-              className="h-full w-1/2 object-cover"
-            />
-            <img
-              src="/images/bg-yarn-1.png"
-              alt="Aiguilles à tricoter"
-              className="h-full w-1/2 object-cover"
-            />
-          </div>
-          <div className="flex h-full w-1/3">
-            <img
-              src="/images/bg-yarn-1.png"
-              alt="Tricot"
-              className="h-full w-1/2 object-cover"
-            />
-            <img
-              src="/images/bg-yarn-1.png"
-              alt="Pelotes de laine"
-              className="h-full w-1/2 object-cover"
-            />
-          </div>
-          <div className="flex h-full w-1/3">
-            <img
-              src="/images/bg-yarn-1.png"
-              alt="Aiguilles à tricoter"
-              className="h-full w-1/2 object-cover"
-            />
-            <img
-              src="/images/bg-yarn-1.png"
-              alt="Tricot"
-              className="h-full w-1/2 object-cover"
-            />
-          </div>
-        </div>
-      </div>
+    <main className="min-h-screen">
       <div className="mx-auto max-w-5xl px-4 py-8">
         {/* Header */}
         <header className="mb-8 flex flex-row items-start justify-between gap-4">
